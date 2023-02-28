a32bc12ae1f1f3b31bbc435306a4d18ecf5bc86a
rgiacomini <rafranca2005@gmail.com>
2023-02-28 19:30:59 -0300

    TesteDeSecreat02

--- src/config/geral copy.php
+++ src/config/geral copy.php
@@ -7,10 +7,7 @@ $DB_USER = $_ENV['DB_USER'];
 $DB_PASS = $_ENV['DB_PASS'];
 $DB_NAME = $_ENV['DB_NAME'];
 
-$DB_HOST2 = ['DB_HOST2'];
-$DB_USER2 = ['Administrator'];
-$DB_PASS2 = ['DB_!K22!XKFh73RiMw@sF3mkpd'];
-$DB_NAME2 = ['db_access'];
+$Git_token = ['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']
 
 // Configuração de diretório
 define('USER_IMG_PATH', $_ENV['USER_IMG_PATH']);
