d5a8871e7cc75f33ef8dca0343757155cdfbadd1
rgiacomini <rafranca2005@gmail.com>
2023-02-28 20:41:19 -0300

    Test_ArqEnv-28022023

---
+++ src/config/env.php
@@ -0,0 +1,10 @@
+<?php
+// Configuração do banco
+$_ENV['DB_HOST'] = '127.0.0.1';
+$_ENV['DB_USER'] = 'user';
+$_ENV['DB_PASS'] = 'password';
+$_ENV['DB_NAME'] = 'Forum';
+
+// Configuração de diretório
+$_ENV['USER_IMG_PATH'] = '/var/www/html/uploads/';
+?>
